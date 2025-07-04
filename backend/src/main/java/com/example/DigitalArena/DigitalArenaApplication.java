4822b82249ccce9d32de35789c612ce3b8b66266
half_Owl <rohitdahiya102852@gmail.com>
2025-07-04 11:38:31 +0530

    First Commit. Basic Setup

---
+++ backend/src/main/java/com/example/DigitalArena/DigitalArenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DigitalArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalArenaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/DigitalArena/DigitalArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DigitalArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
